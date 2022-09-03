1d449d44e01787ef17dc7b0672eec01a8121b36e
Xavier Valarino <xvalarino@gmail.com>
2022-09-03 05:06:04 -0700

    fix: mapping when right-hand side is `nil` (#323)
    Only seems to occur with '!' on the left-hand side

--- lua/which-key/keys.lua
+++ lua/which-key/keys.lua
@@ -254,7 +254,7 @@ function M.map(mode, prefix_n, cmd, buf, opts)
   local other = vim.api.nvim_buf_call(buf or 0, function()
     local ret = vim.fn.maparg(prefix_n, mode, false, true)
     ---@diagnostic disable-next-line: undefined-field
-    return (ret and ret.lhs and ret.rhs ~= cmd) and ret or nil
+    return (ret and ret.lhs and ret.rhs and ret.rhs ~= cmd) and ret or nil
   end)
   if other then
     table.insert(M.duplicates, { mode = mode, prefix = prefix_n, cmd = cmd, buf = buf, other = other })
